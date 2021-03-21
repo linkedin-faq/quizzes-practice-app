a788f05bd7a2ce15f69da51b84eb630f896466f5
hemant negi <nhemnt@gmail.com>
2021-03-22 02:19:28 +0530

    change primary theme to grey

--- src/theme.js
+++ src/theme.js
@@ -1,11 +1,11 @@
 import { createMuiTheme } from '@material-ui/core/styles';
-import purple from '@material-ui/core/colors/purple';
+import grey from '@material-ui/core/colors/grey';
 import green from '@material-ui/core/colors/green';
 
 export const theme = createMuiTheme({
   palette: {
     primary: {
-      main: purple[500],
+      main: grey[500],
     },
     secondary: {
       main: green[500],
