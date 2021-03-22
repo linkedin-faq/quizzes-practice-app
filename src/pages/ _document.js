dd2e96f48b75bb9f92ae5706166cf054e01164d4
hemant negi <nhemnt@gmail.com>
2021-03-22 22:50:49 +0530

    fix default export build fail

--- src/pages/ _document.js
+++ src/pages/ _document.js
@@ -3,7 +3,7 @@ import Document, {
   Html,Head, Main, NextScript,
 } from 'next/document';
 import { ServerStyleSheets } from '@material-ui/core/styles';
-import theme from 'theme';
+import { theme } from 'theme';
 export default class MyDocument extends Document {
   render() {
     return (
